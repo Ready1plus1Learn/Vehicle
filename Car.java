4757e6f8b044ae03b81eee245f435292dbd6bd86
David Christian <97576285+Ready1plus1Learn@users.noreply.github.com>
2024-05-04 01:18:04 +0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+public class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details: ");
+        System.out.println("Make: " + getMake());
+        System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getYear());
+        System.out.println("Number of Doors: " + getNumberOfDoors());
+    }
+}
